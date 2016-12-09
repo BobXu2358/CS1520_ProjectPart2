1f98d5d21c552dec2393943a93190854c02b98b4
BobXu2358 <xxh2358@gmail.com>
2016-12-08 21:11:10 -0500

    use php to reduce code repreat

--- php/place.php
+++ php/place.php
@@ -10,34 +10,7 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-inverse navbar-static-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="../index.php">Xinhai's Site</a>
-      </div>
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="../php/myself.php">About Me</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Hometown<span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="../php/place.php">Where is it</a></li>
-              <li><a href="#">How does it look like</a></li>
-              <li><a href="#">Why I love it</a></li>
-            </ul>
-          </li>
-          <li><a href="../php/portfolio.php">Demos</a></li>
-          <li><a href="../php/form.php">Contact</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <?php include '../html/navbar.html';?>
   <div class="container" style="color: lavender;">
     <h2>My Hometown</h2>
     <ul class="nav nav-tabs">
